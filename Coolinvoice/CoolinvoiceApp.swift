c3547cff3b381b51e9a23150f7961173d51a38a0
Halis Can <haliscan@hotmail.com>
2025-12-02 17:49:08 -0500

    Initial Commit

---
+++ Coolinvoice/CoolinvoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolinvoiceApp.swift
+//  Coolinvoice
+//
+//  Created by Ozde Can on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CoolinvoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoolinvoiceTests/CoolinvoiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoolinvoiceTests.swift
+//  CoolinvoiceTests
+//
+//  Created by Ozde Can on 12/2/25.
+//
+
+import Testing
+@testable import Coolinvoice
+
+struct CoolinvoiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
